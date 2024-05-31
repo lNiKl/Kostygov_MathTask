0b7c67429087930c24923e7826768e428b91259e
kostygovnv <kolyakostygov@gmail.com>
2024-05-31 08:49:35 +0300

    User input check

--- Kostygov_MathTask/Kostygov_MathTask.cpp
+++ Kostygov_MathTask/Kostygov_MathTask.cpp
@@ -1,6 +1,20 @@
 ﻿#include <iostream>
-
+#include <string>
+using namespace std;
 int main()
 {
     std::cout << "Hello World!\n";
+}
+bool User_Inpyt(string input)
+{
+    if (input.empty()) return false;
+	try
+	{
+		int number = stoi(input);
+	}
+	catch (...)
+	{
+		return false;
+	}
+	return true;
 }
